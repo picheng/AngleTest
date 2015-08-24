9e68c30013b5a413d24a975d02c22e31321a2f1a
picheng <picheng2007@gmail.com>
2015-08-24 16:02:22 +0800

    111

---
+++ AngleTest/src/com/angle/test/TestMain.java
@@ -0,0 +1,10 @@
+package com.angle.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is Angle test");
+	}
+
+}
